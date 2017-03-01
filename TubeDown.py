9c3ffa8c2daf25dd858d11e92c9e005e2ce519af
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-03-01 01:57:09 -0600

    [+] Actualizado
    Actualizado y Comentado

--- TubeDown.py
+++ TubeDown.py
@@ -1,3 +1,17 @@
+# Lista De Casos De Posibles Usos:
+#~ print("\n\n", yt.get_videos())
+#~ print("\n\n", yt.filename)
+#~ yt.set_filename('Video 1 - Metasploit Framework') #~ Para renombrar el archivo de salida
+#~ print("\n\n", yt.filename)
+#~ print("\n\n", yt.filter('mp4'))
+#~ print("\n\n", yt.filter('mp4')[-1])
+#~ print("\n\n", yt.filter(resolution='720p'))
+#~ print("\n\n", video)
+#~ print("\n\n", yt.videos)
+#~ video = yt.get('mp4')
+#~ print("\n\n", VideoHD)
+
+#~ ================================================================================================
 import os
 
 
@@ -16,6 +30,7 @@ def Chk_Dep():
 	except Exception as ex:
 		print( type(ex).__name__ )		#Ver cuando ocurre un error y poder añadirlo a las ecepciones, y no cierre el programa.
 
+#~ os.system("pip uninstall pytube -y")# > Nul")
 
 PT = True
 Chk_Dep()
@@ -41,12 +56,15 @@ xD = True
 
 while xD:
 	
-	NomVid = input("URL: ")
+	NomVid = input("URL: ")				#~ Escribimos la URL del Video a Descargar.
 
-	Video = YouTube(NomVid)
+	Video = YouTube(NomVid)					#~ Se Obtienen Todas Las Calidades Posibles De Ese Video.
 
-	VideoHD = Video.get('mp4', '720p')
-
-	VideoHD.download(r""+Ruta)
+	#~ VideoHD = Video.get('mp4', '720p')		#Obtenemos el video mp4 de 720p.
+	VideoHD = Video.filter('mp4')[-1]			#Obtenemos el video mp4 de mejor calidad posible.
+	
+	print("\n\n\n Video: ", VideoHD.filename)		#Se imprime el nombre del video.
+	
+	#~ VideoHD.download(r""+Ruta)				#Descargamos el video seleccionado.
 	
 	
