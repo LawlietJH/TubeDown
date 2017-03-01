b46dea436b2d4a5b5554d9e7c203a857a1e6acaa
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-03-01 01:44:47 -0600

    Código Base
    Añadido Código Base

---
+++ TubeDown.py
@@ -0,0 +1,23 @@
+import os
+
+
+def Chk_Dep():
+	
+	try:
+		import pytube
+		
+	except ModuleNotFoundError:
+		print("\n\n\t[!] Instalando Dependencias...\n\n\t\t")
+		os.system("pip install pytube")# > Nul && cls")
+		global PT
+		PT = False
+		return PT
+		
+	except Exception as ex:
+		print( type(ex).__name__ )		#Ver cuando ocurre un error y poder añadirlo a las ecepciones, y no cierre el programa.
+
+
+PT = True
+Chk_Dep()
+if PT: from pytube import *
+
